aaf05be8285a3eca27f36a89cb64c1af0fc7f48c
Jayden Skarbek <jaydenskarbek@gmail.com>
2025-10-23 14:22:28 -0400

    Implement login endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
-// app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your frontend's origin
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}));
+//app.use(bodyParser.json());
 app.use(express.json());
 app.use((req, res, next) => {
     app.get("/api/ping", (req, res, next) => {
@@ -20,4 +24,27 @@ app.use((req, res, next) => {
     );
     next();
 });
-app.listen(5000);
+
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+    var error = '';
+    const { login, password } = req.body;
+    var id = -1;
+    var fn = '';
+    var ln = '';
+    if (login.toLowerCase() == 'rickl' && password == 'COP4331') {
+        id = 1;
+        fn = 'Rick';
+        ln = 'Leinecker';
+    }
+    else {
+        error = 'Invalid user name/password';
+    }
+    var ret = { id: id, firstName: fn, lastName: ln, error: error };
+    res.status(200).json(ret);
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
