b615219f2412bf62969f8a71b94c56aa4aca1d6a
Faith Munar <faith.munar01@icloud.com>
2025-10-23 14:39:06 -0400

    change some thing to the login.css and login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -7,13 +7,11 @@ function Login() {
     }
     return (
         <div id="loginDiv">
-            <span id="inner-title">PLEASE LOG IN</span><br />
+            <span id="inner-title">Login</span><br />
             <input type="text" id="loginName" placeholder="Username" /><br />
             <input type="password" id="loginPassword" placeholder="Password" /><br />
             <input type="submit" id="loginButton" className="buttons" value="Login"
-                onClick={doLogin} />
-            <input type="submit" id="registerButton" className="buttons" value="Register New Account"
-                /*onClick={doRegister}*/ />    
+                onClick={doLogin} />    
             <span id="loginResult"></span>
         </div>
     );
