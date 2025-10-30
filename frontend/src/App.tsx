e8425649800177c959c41fdec54ee067cf7b253c
Alan Fairman <129026438+MeatBoy21@users.noreply.github.com>
2025-10-29 22:37:15 -0400

    Refactor so it runs on server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-dom';
 import './App.css';
+
 import LoginPage from './pages/LoginPage.tsx';
 import RegisterPage from './pages/RegisterPage.tsx';
-import CardPage from './pages/CardPage.tsx';
+
 function App() {
   return (
-    <RegisterPage />
+    <Router >
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
