f7d1064fecb7cca80442e185e9d4c4fc3b8a5647
Alan Fairman <129026438+MeatBoy21@users.noreply.github.com>
2025-10-29 22:39:47 -0400

    Connects to mogoDB
    TODO: Add appropriate collection and field names

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://hugoputigna:SzyF0sJk6Z40f1Uh@cardcluster.eup3fgb.mongodb.net/?retryWrites=true&w=majority&appName=CardCluster';
+const client = new MongoClient(url);
+client.connect();
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend's origin
+    origin: 'http://134.199.193.253:3000/', // Replace with your frontend's origin
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 }));
-//app.use(bodyParser.json());
+//app.use(bodyParser.json())
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log('MongoDB Connected');
+  } catch(err) {
+    console.error('MongoDB Connection Failed');
+  }
+}
+connectDB();
+
 app.use(express.json());
 app.use((req, res, next) => {
     app.get("/api/ping", (req, res, next) => {
@@ -30,20 +45,20 @@ app.post('/api/login', async (req, res, next) => {
     // outgoing: id, firstName, lastName, error
     var error = '';
     const { login, password } = req.body;
+    const db = client.db('COP4331Cards');
+    const results = await db.collection('users').find({ Login: login, Password: password }).toArray();
     var id = -1;
     var fn = '';
     var ln = '';
-    if (login.toLowerCase() == 'rickl' && password == 'COP4331') {
-        id = 1;
-        fn = 'Rick';
-        ln = 'Leinecker';
+    if (results.length > 0) {
+        id = results[0].UserID;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
     }
-    else {
-        error = 'Invalid user name/password';
-    }
-    var ret = { id: id, firstName: fn, lastName: ln, error: error };
+    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
     res.status(200).json(ret);
 });
+
 
 app.post('/api/register', (req, res) => {
     const { login, password, firstName, lastName} = req.body;
