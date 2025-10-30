5671ac7b1fb692bec7be5797f94e9a64440e681f
Alan Fairman <129026438+MeatBoy21@users.noreply.github.com>
2025-10-29 22:45:40 -0400

    Changed frontend port to be distinct from backend port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: '134.199.193.253', // Listen on all network interfaces
-    port: 5000,
+    port: 5100,
     allowedHosts: true,
     plugins: [react()]
   },
