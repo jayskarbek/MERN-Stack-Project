77837b5082d951f8b806fdeaca9c307dd039d11b
Faith Munar <faith.munar01@icloud.com>
2025-11-06 14:41:09 -0500

    forgot password css and page link

---
+++ frontend/src/pages/ForgotPassPage.tsx
@@ -0,0 +1,11 @@
+import ForgotPassword from '../components/ForgotPassword.tsx';
+
+const ForgotPassPage = () => {
+    return (
+        <div>
+            <ForgotPassword />
+        </div>
+    );
+};
+
+export default ForgotPassPage;
