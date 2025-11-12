268c710629295998c6651f9d1519168fe1e32e7c
jayskarbek <jaydenskarbek@gmail.com>
2025-11-12 00:39:33 -0500

    Edited regist and password api to fix transporter for nodemailer

--- backend/routes/password.js
+++ backend/routes/password.js
@@ -33,7 +33,9 @@ module.exports = function (db) {
             )
 
             const transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true, // use SSL
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASSWORD
